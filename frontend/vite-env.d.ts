adc7b7356f96d9df8940b12586c8dfaea75146e4
devakeerthanapradeep <devakeerthana9112gmail.com>
2025-11-22 15:52:54 +0530

    updated tsconfig

---
+++ frontend/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+// Add any custom environment variables here for TypeScript.
+// Example:
+// interface ImportMetaEnv {
+//   readonly VITE_API_BASE_URL: string;
+// }
+//
+// interface ImportMeta {
+//   readonly env: ImportMetaEnv;
+// }
